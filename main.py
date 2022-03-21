b128ce5d6aba76695edb340897ccd21297ab1097
Anya <anna.artamono@gmail.com>
2022-03-21 03:04:16 +0300

    Поиск data для введённого слова

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, make_response, render_template, request, redirect
+from requests import get as get_request
 
 from config import *
 
@@ -7,7 +8,7 @@ app.config['SECRET_KEY'] = FLASK_KEY
 
 
 def main():
-    app.run(port=8080, host='127.0.0.1')
+    app.run(port=8080, host='0.0.0.0')
 
 
 def search_word():
@@ -19,20 +20,33 @@ def search_word():
 @app.route('/', methods=['POST', 'GET'])
 def start():
     if request.method == 'POST':
-        search_word()
+        return search_word()
 
-    elif request.method == 'GET':
-        return render_template('main.html')
+    return render_template('main.html')
 
 
 @app.route('/search=<params>', methods=['POST', 'GET'])
 def search(params):
     if request.method == 'POST':
-        search_word()
+        return search_word()
 
-    data = params
+    api_server = f'https://owlbot.info/api/v4/dictionary/{params}'
+    headers = {'Authorization': 'Token ' + OWL_API_TOKEN}
+    response = get_request(api_server, headers=headers)
+    owl_api_response = response.json()
+
+    api_server = f'https://imsea.herokuapp.com/api/1?q={params}'
+    response = get_request(api_server)
+    pics_response = response.json()['results'][:2]
+
+    api_server = f'https://api.dictionaryapi.dev/api/v2/entries/en/{params}'
+    response = get_request(api_server)
+    dict_api_response = response.json()
+
+    data = [owl_api_response, pics_response, dict_api_response]
+    print(data)
     return render_template('search_result.html', title='Результаты поиска',
-                           data=params)
+                           data=data, search=' '.join(params.split('+')))
 
 
 main()
