6f0c90153df1f6bfc7d917cd93ff3de95fe5c963
Anya <anna.artamono@gmail.com>
2022-04-11 20:26:18 +0300

    Начало работы

--- main.py
+++ main.py
@@ -25,28 +25,36 @@ def start():
     return render_template('main.html')
 
 
-@app.route('/search=<params>', methods=['POST', 'GET'])
-def search(params):
-    if request.method == 'POST':
-        return search_word()
+@app.route('/profile')
+def profile_default():
+    """ """
+    return redirect(f'/login')
+    # # Если пользователь авторизован, то перенаправляем его на страницу по id
+    # if ...:
+    #     pass
+    # # Иначе - перенаправляем на страницу для регистрации/авторизации
+    # else:
+    #     pass
 
-    api_server = f'https://owlbot.info/api/v4/dictionary/{params}'
-    headers = {'Authorization': 'Token ' + OWL_API_TOKEN}
-    response = get_request(api_server, headers=headers)
-    owl_api_response = response.json()
 
-    api_server = f'https://imsea.herokuapp.com/api/1?q={params}'
-    response = get_request(api_server)
-    pics_response = response.json()['results'][:2]
+@app.route('/profile/<int:id>')
+def profile(id):
+    return make_response(f'User {id}')
 
-    api_server = f'https://api.dictionaryapi.dev/api/v2/entries/en/{params}'
-    response = get_request(api_server)
-    dict_api_response = response.json()
 
-    data = [owl_api_response, pics_response, dict_api_response]
-    print(data)
-    return render_template('search_result.html', title='Результаты поиска',
-                           data=data, search=' '.join(params.split('+')))
+@app.route('/login')
+def login():
+    return make_response(f'login page')
+
+
+@app.route('/play')
+def play():
+    return make_response('Play')
+
+
+@app.route('/top')
+def top():
+    return make_response(f'Top of users')
 
 
 main()
