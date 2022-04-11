e967bb1589ad16b8282e8184ecc2873a2b868932
chepyr <72972204+chepyr@users.noreply.github.com>
2022-04-12 01:16:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,8 @@ login_manager.init_app(app)
 
 def main():
     db_session.global_init("db/users.db")
-    app.run(port=8080, host='0.0.0.0')
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
 
 def search_word():
